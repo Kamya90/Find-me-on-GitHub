033dd65e1e445c3ca5b58aa768bc27e42ed8e752
Nagpal <k.a.nagpal@accenture.com>
2023-12-13 18:48:07 +0530

    Push existing project to github

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment, Component } from "react";
+import React, { Fragment, useState } from "react";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
@@ -8,14 +8,14 @@ import Alert from "./components/layout/Alert";
 import About from "./components/pages/About";
 import User from "./components/users/User";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-class App extends Component {
-  state = {
-    users: [],
-    user: {},
-    loading: false,
-    alert: null,
- // username : '' //we'll be needing some setState shit!!!!!!
-  };
+const App =() =>{
+  const [users,setUsers]=useState([]);
+  const [user,setUser]=useState({});
+  const [repos,setRepos]=useState([]);
+  const [loading,setLoading]=useState(false);
+  const [alert,setAlert]=useState(null);
+   
+  ;
 
   // async componentDidMount(){
   //   this.setState({loading:true});
@@ -23,74 +23,59 @@ class App extends Component {
   //   this.setState({users:(await res).data, loading:false});
   // }
 
-  searchUsers = async text => {
-    this.setState({ loading: true });
+  const searchUsers = async text => {
+    setLoading(true);
     const res = await axios.get(
       `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
    
-    this.setState({ users: res.data.items, loading: false });
+    setUsers(res.data.items);
+    setLoading(false);
   };
-  getUser = async (username) => {
-    // this.setState({ loading: true });
+  //get single github user
+  const getUser = async (username) => {
+    setLoading(true);
     try {
       const res = await axios.get(
         `https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
       );
-      console.log("HEREEEE ", res.data)
-    this.setState({ user: res.data });
+      setUser(res.data);
+      setLoading(false);
+    
     } catch (error) {
       console.log("API FAILED ", error)
     }
-
-    // console.log("USER RES ", res)
-    const mockUser = {
-      "login": "octocat",
-      "id": 1,
-      "node_id": "MDQ6VXNlcjE=",
-      "avatar_url": "https://github.com/images/error/octocat_happy.gif",
-      "gravatar_id": "",
-      "url": "https://api.github.com/users/octocat",
-      "html_url": "https://github.com/octocat",
-      "followers_url": "https://api.github.com/users/octocat/followers",
-      "following_url": "https://api.github.com/users/octocat/following{/other_user}",
-      "gists_url": "https://api.github.com/users/octocat/gists{/gist_id}",
-      "starred_url": "https://api.github.com/users/octocat/starred{/owner}{/repo}",
-      "subscriptions_url": "https://api.github.com/users/octocat/subscriptions",
-      "organizations_url": "https://api.github.com/users/octocat/orgs",
-      "repos_url": "https://api.github.com/users/octocat/repos",
-      "events_url": "https://api.github.com/users/octocat/events{/privacy}",
-      "received_events_url": "https://api.github.com/users/octocat/received_events",
-      "type": "User",
-      "site_admin": false,
-      "name": "monalisa octocat",
-      "company": "GitHub",
-      "blog": "https://github.com/blog",
-      "location": "San Francisco",
-      "email": "octocat@github.com",
-      "hireable": true,
-      "bio": "There once was...",
-      "twitter_username": "monatheoctocat",
-      "public_repos": 2,
-      "public_gists": 1,
-      "followers": 20,
-      "following": 0,
-      "created_at": "2008-01-14T04:33:35Z",
-      "updated_at": "2008-01-14T04:33:35Z"
+  };
+  //get user repos
+  const getUserRepos = async (username) => {
+    setLoading(true);
+    try {
+      const res = await axios.get(
+        `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      );
+      //console.log("HEREEEE ", res.data)
+    setRepos(res.data);
+    setLoading(false);
+    } catch (error) {
+      console.log("API FAILED ", error)
     }
   };
-  clearUsers = () => {
-    this.setState({ users: [], loading: false });
+
+  //clear users from state
+ const clearUsers = () => {
+    setUsers([]);
+    setLoading(false);
   };
 
-  setAlert = (msg, type) => {
-    this.setState({ alert: { msg, type } });
-    setTimeout(() => this.setState({ alert: null }), 5000);
+  //set alert
+  const showAlert = (msg, type) => {
+    setAlert({msg,type});
+    setTimeout(() => setAlert(null),5000);
   };
 
-  render() {
+  
 
-    const {users, user, loading } = this.state;
+   // const {users, user,repos, loading } = this.state;
 
     return (
       <Router>
@@ -99,7 +84,7 @@ class App extends Component {
 
           <div className="container">
             
-            <Alert alert={this.state.alert} />
+            <Alert alert={alert} />
             <Routes>
               <Route
                 exact
@@ -110,10 +95,10 @@ class App extends Component {
                   return (
                     <Fragment>
                       <Search
-                        searchUsers={this.searchUsers}
-                        clearUsers={this.clearUsers}
+                        searchUsers={searchUsers}
+                        clearUsers={clearUsers}
                         showClear={users && users.length > 0 ? true : false}
-                        setAlert={this.setAlert}
+                        setAlert={showAlert}
                       />
                      
                       <Users loading={loading} users={users} />
@@ -138,7 +123,9 @@ class App extends Component {
                   console.log("HERE", user);
                   return (
                     <User
-                    getUser={this.getUser}
+                    getUser={getUser}
+                    getUserRepos={getUserRepos}
+                    repos={repos }
                     username={username}
                     user={user}
                     loading={loading}
@@ -151,7 +138,8 @@ class App extends Component {
         </div>
       </Router>
     );
-  }
+  
 }
+
 
 export default App;
--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,29 +1,31 @@
-import React, { Fragment, Component } from "react";
+import React, { Fragment, useEffect } from "react";
 import Spinner from "../layout/Spinner";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import Repos from "../repos/Repos";
 
-export class User extends Component {
+const User =({user, loading, getUser, getUserRepos,repos,username})=> {
   // componentDidMount(){
   //   this.props.getUser(this.props.match.params.login);
   // }
-  componentDidMount() {
-    const username = this.props.username;
-    //console.log("MY USER ", this.props.user.login)
-    if (username !== this.props.user.login) {
-      this.props.getUser(username);
+  useEffect(()=>{
+    // const username = username;
+    //const repos=repos;
+   
+    if (username !== user.login) {
+      getUser(username);
+      getUserRepos(username);
     }
-    //console.log("text", this.props.match.params.login);
-  }
+    // eslint-disable-next-line
+  },[]);
+  
 
-  static propTypes = {
-    loading: PropTypes.bool,
-    user: PropTypes.object.isRequired,
-    getUser: PropTypes.func.isRequired,
-  };
+  
 
-  render() {
+ 
     const {
+      name,
+      company,
       avatar_url,
       location,
       bio,
@@ -35,13 +37,13 @@ export class User extends Component {
       public_repos,
       public_gists,
       hireable,
-    } = this.props.user;
+    } = user;
 
-    const { loading } = this.props;
-    // if (loading)
-    //   return <Spinner/>
-    //   else{
-    console.log("RENDERRRR ", this.props.user);
+    // const { loading, repos } = this.props;
+    if (loading)
+     return <Spinner/>
+    else{
+   // console.log("RENDERRRR ", this.props.user);
 
     return (
       <Fragment>
@@ -55,11 +57,65 @@ export class User extends Component {
         ) : (
           <i className="fas fa-check text-danger" />
         )}
+        
+        <div className="card grid-2">
+          <div className="all-center">
+            <img src={avatar_url} className="round-img" alt="" style={{width:'150px'}}/>
+            <h1>{name}</h1>
+            <p>Location:{location}</p>
+          </div>
+          <div>
+          {bio && 
+          <Fragment>
+            <h3>Bio</h3>
+            <p>{bio}</p>
+            </Fragment>
+            }
+            <a href={html_url} className="btn btn-dark my-1">Visit my GitHub</a>
+            <ul>
+              <li>
+                {login && <Fragment>
+                  <strong>Username:</strong>
+                  {login}
+                  </Fragment>}
+              </li>
+              <li>
+                {company && <Fragment>
+                  <strong>Company:</strong>
+                  {company}
+                  </Fragment>}
+              </li>
+              <li>
+                {blog && <Fragment>
+                  <strong>Website:</strong>
+                  {blog}
+                  </Fragment>}
+              </li>
+            </ul>
+        </div>
+        </div>
+
+        <div className="card text-center">
+          <div className="badge badge-primary">Followers: {followers}</div>
+          <div className="badge badge-success">Following: {following}</div>
+          <div className="badge badge-light">Public Repos: {public_repos}</div>
+          <div className="badge badge-dark">Public Gists: {public_gists}</div>
+        </div>
+        <Repos repos={repos}/>
       </Fragment>
     );
 
-    // }
-  }
+     }
+  
+}
+User.propTypes={
+ 
+    loading: PropTypes.bool,
+    user: PropTypes.object.isRequired, 
+    getUser: PropTypes.func.isRequired,
+    getUserRepos:PropTypes.func.isRequired,
+    repos:PropTypes.array.isRequired
+  
 }
 
 export default User;
